fbbdc81938934b59d5d4e1873a9e08647f00a246
Mif Masterz <mifmasterz@yahoo.com>
2021-05-28 20:10:15 +0700

    update to 2.1 rc1

--- src/TestFatFS/Program.cs
+++ src/TestFatFS/Program.cs
@@ -24,20 +24,22 @@ namespace TestFatFS
 
         static void TestNewApi()
         {
-            var drive = FileSystem.Mount(GHIElectronics.TinyCLR.Pins.SC20260.SpiBus.Spi2, GHIElectronics.TinyCLR.Pins.SC20260.GpioPin.PA13, GHIElectronics.TinyCLR.Pins.SC20100.GpioPin.PC13);
+            var drive = FileSystem.Mount(GHIElectronics.TinyCLR.Pins.SC13048.SpiBus.Spi1, GHIElectronics.TinyCLR.Pins.SC13048.GpioPin.PB2, GHIElectronics.TinyCLR.Pins.SC13048.GpioPin.PA0);
 
             //Show a list of files in the root directory
             var directory = new DirectoryInfo(drive.Name);
-
+            var subdir = directory.CreateSubdirectory("data");
             var files = directory.GetFiles();
 
             foreach (var f in files)
             {
+                //f.Delete();
                 System.Diagnostics.Debug.WriteLine(f.Name);
             }
 
             //Create a text file and save it to the SD card.
-            var file = new FileStream($@"{drive.Name}Test1.txt", FileMode.OpenOrCreate);
+            //var file = new FileStream($@"{drive.Name}Test1.txt", FileMode.OpenOrCreate);
+            var file = new FileStream($@"{subdir.FullName}\\Test2.txt", FileMode.OpenOrCreate);
             var bytes = Encoding.UTF8.GetBytes(DateTime.UtcNow.ToString() +
                 Environment.NewLine);
 
